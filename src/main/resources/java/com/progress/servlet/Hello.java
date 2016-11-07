c9b9cf88ab1ab6e64b5a8234f8ae264aea2070c1
KingYL <1028601764@qq.com>
2016-11-07 14:24:23 +0800

    测试

--- src/main/resources/java/com/progress/servlet/Hello.java
+++ src/main/resources/java/com/progress/servlet/Hello.java
@@ -25,7 +25,8 @@ public class Hello extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().append("为了方圆做的测试.").append(request.getContextPath());
 	}
 
 	/**
